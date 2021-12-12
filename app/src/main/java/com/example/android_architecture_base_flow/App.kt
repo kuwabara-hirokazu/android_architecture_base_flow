7ea373931de65a5c033ca0f65211c149bd1e0732
kuwabara-hirokazu <h.kuwabara1103@gmail.com>
2021-12-12 20:18:12 +0900

    Hilt導入

---
+++ app/src/main/java/com/example/android_architecture_base_flow/App.kt
@@ -0,0 +1,8 @@
+package com.example.android_architecture_base_flow
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+}
--- app/src/main/java/com/example/android_architecture_base_flow/MainActivity.kt
+++ app/src/main/java/com/example/android_architecture_base_flow/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.android_architecture_base_flow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
