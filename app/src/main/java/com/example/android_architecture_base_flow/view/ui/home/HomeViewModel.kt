aa14bb85e5c2a6461532d5397de12ddb70063020
kuwabara-hirokazu <h.kuwabara1103@gmail.com>
2021-12-12 20:58:53 +0900

    BaseViewModelを実装

--- app/src/main/java/com/example/android_architecture_base_flow/view/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/android_architecture_base_flow/view/ui/home/HomeViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.android_architecture_base_flow.view.ui.home
 
+import com.example.android_architecture_base_flow.util.InjectionUtil
 import com.example.android_architecture_base_flow.view.ui.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : BaseViewModel() {
+class HomeViewModel @Inject constructor(
+    injectionUtil: InjectionUtil
+) : BaseViewModel(injectionUtil) {
 }
